49a67392fa3f771b94bf27c728144d7d01e82409
Igor1II1 <jmuxjmux5@gmail.com>
2025-06-16 14:48:23 +1000

    Initial commit

---
+++ vue-sneakers/eslint.config.js
@@ -0,0 +1,40 @@
+import js from '@eslint/js'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import pluginOxlint from 'eslint-plugin-oxlint'
+import pluginVue from 'eslint-plugin-vue'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
+
+export default defineConfig([
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{js,mjs,jsx,vue}'],
+  },
+
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+
+  js.configs.recommended,
+  ...pluginVue.configs['flat/essential'].map((config) => ({
+    ...config,
+    settings: {
+      'vue.version': '3.0',
+    },
+  })),
+  ...pluginOxlint.configs['flat/recommended'],
+  skipFormatting,
+
+  {
+    rules: {
+      'vue/no-multiple-template-root': 'off',
+      'vue/multi-word-component-names': 'off',
+    },
+  },
+])
